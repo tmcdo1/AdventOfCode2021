a5dad12a223b6e43ec79eb98cfb02e25b3cadff6
Reid Lindemann <reid.lindemann@angi.com>
2021-12-09 23:47:48 -0600

    Day 10 Part 1

--- day10/reid/day10a.py
+++ day10/reid/day10a.py
@@ -2,25 +2,27 @@ lines = []
 with open('inputs/day10.txt') as f:
     lines = f.readlines()
 
-grid = []
-i = 0
+sumPoints = 0
 for line in lines:
-    grid.append(list(line.strip()))
-    i += 1
+    stack = []
+    for c in list(line.strip()):
+        if c == "(":
+            stack.append(")")
+        elif c == "[":
+            stack.append("]")
+        elif c == "{":
+            stack.append("}")
+        elif c == "<":
+            stack.append(">")
+        else:
+            if c != stack.pop():
+                if c == ")":
+                    sumPoints += 3
+                elif c == "]":
+                    sumPoints += 57
+                elif c == "}":
+                    sumPoints += 1197
+                elif c == ">":
+                    sumPoints += 25137
 
-sumRisk = 0
-for y in range(0, len(grid)):
-    for x in range(0, len(grid[y])):
-        lowPoint = True
-        if y < len(grid)-1 and int(grid[y][x]) >= int(grid[y+1][x]):
-            lowPoint = False
-        if y > 0 and int(grid[y][x]) >= int(grid[y-1][x]):
-            lowPoint = False
-        if x < len(grid[y])-1 and int(grid[y][x]) >= int(grid[y][x+1]):
-            lowPoint = False
-        if x > 0 and int(grid[y][x]) >= int(grid[y][x-1]):
-            lowPoint = False
-        if lowPoint:
-            sumRisk += int(grid[y][x]) + 1
-
-print(sumRisk)
+print(sumPoints)
